cbcd6ab76cccaff5719979d3babf3d7f8eaae3d0
jgaruti <56049198+jgaruti@users.noreply.github.com>
2020-12-05 20:29:20 -0500

    Add files via upload
    The files that I have for the project (creating a database for a library.)

---
+++ LibraryProject/names.sql
@@ -0,0 +1,21 @@
+BEGIN;
+INSERT INTO names (nameID, name) VALUES
+(0, 'Joseph'),
+(1, 'Michael'),
+(2, 'Garuti'),
+(3, 'Ivanildo'),
+(4, 'Araujo'),
+(5, 'Sarah'),
+(6, 'Gordon'),
+(7, 'Jessica'),
+(8, 'Colleen'),
+(9, 'Dan'),
+(10, 'Andrews'),
+(11, 'Shirley'),
+(12, 'May'),
+(13, 'Cunningham'),
+(14, 'Jo-Ann'),
+(15, 'Debrah'),
+(16, 'Stephen'),
+(17, 'Rogers')
+;
